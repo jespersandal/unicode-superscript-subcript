2eeb5e28786b0303b9507ddcd9e0d1e22dca7d38
Jesper S. Sandal <jespersandal@outlook.com>
2016-08-31 11:15:31 +0200

    file initialization
    Added example.html and script files. Unifinished content. Copied in
    Unicode characters in the two scripts.

---
+++ unisub.js
@@ -0,0 +1,4 @@
+function unisub(var orisub) {
+  var unicodesubchars = ["₀","₁","₂","₃","₄","₅","₆","₇","₈","₉"];
+  
+}
---
+++ unisup.js
@@ -0,0 +1,3 @@
+function unisup(var orisup){
+  var unicodesupchars = ["⁰","¹","²","³","⁴","⁵","⁶","⁷","⁸","⁹","ⁿ",""];
+}
